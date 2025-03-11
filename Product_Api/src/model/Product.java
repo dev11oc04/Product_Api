7455a033149ceb5fca01e93d342f4b4b8df1b78e
dev11oc04 <134136651+dev11oc04@users.noreply.github.com>
2025-03-11 21:33:26 +0530

    Update Product.java

--- Product_Api/src/model/Product.java
+++ Product_Api/src/model/Product.java
@@ -1,1 +1,19 @@
+package com.example.model;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "Product") // MongoDB collection name
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    private String Id; 
+    private String productName;
+    private String description;
+    private double price;
+    private int quantity;
+}
 
