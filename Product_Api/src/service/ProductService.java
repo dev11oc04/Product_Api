e5169a6213c7ae4716a35025c572ab4762e9f70f
dev11oc04 <134136651+dev11oc04@users.noreply.github.com>
2025-03-11 21:32:32 +0530

    Update ProductService.java

--- Product_Api/src/service/ProductService.java
+++ Product_Api/src/service/ProductService.java
@@ -1,1 +1,45 @@
+package com.example.service;
+
+import com.example.model.Product;
+import com.example.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    // Get all employees
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    // Get employee by ID
+    public Optional<Product> getProductById(String id) {
+        return productRepository.findById(id);
+    }
+
+    // Add new employee
+    public Product addProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    // Update employee
+    public Product updateProduct(String id, Product updatedProduct) {
+        if (productRepository.existsById(id)) {
+            updatedProduct.setId(id); // Ensure the ID remains unchanged
+            return productRepository.save(updatedProduct);
+        }
+        return null;
+    }
+
+    // Delete employee
+    public void deleteProduct(String id) {
+        productRepository.deleteById(id);
+    }
+}
 
