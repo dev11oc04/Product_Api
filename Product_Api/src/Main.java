80ca5723c750259a1746776c6837b36ec578b33b
dev11oc04 <134136651+dev11oc04@users.noreply.github.com>
2025-03-11 21:29:34 +0530

    Create Main.java

---
+++ Product_Api/src/Main.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("2211CS050083");
+		SpringApplication.run(Main.class, args);
+	}
+
+}
